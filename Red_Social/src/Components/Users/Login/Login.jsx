54715afdafbd3ca216f7b232a2660e3d23fd2168
hcrj99 <hcrj99@hotmail.com>
2024-05-05 14:57:32 -0500

    upgrdes + struct + style login

--- Red_Social/src/Components/Users/Login/Login.jsx
+++ Red_Social/src/Components/Users/Login/Login.jsx
@@ -1,5 +1,16 @@
 export const Login = () => {
     return (
-        <div>Login</div>
+        <form className='Form__container' >
+            <h2>Welcome!...</h2>
+            <div>
+                <label htmlFor="E-mail">E-mail: </label>
+                <input type="text" name='E-mail' placeholder="E-mail" />
+            </div>
+            <div>
+                <label htmlFor="password">Password: </label>
+                <input type="text" name='password' placeholder="Password" />
+            </div>
+            <input type="submit" value='Login'/>
+        </form>
     )
 }
--- Red_Social/src/Components/Users/Register/Register.jsx
+++ Red_Social/src/Components/Users/Register/Register.jsx
@@ -2,7 +2,7 @@ import './Register.css';
 
 export const Register = () => {
     return (
-        <form className='Form__register'>
+        <form className='Form__container'>
             <h2>Register Now!</h2>
             <div> <label htmlFor="Name" >Name: </label>
                 <input type="text" name='Name' placeholder="Name" />
@@ -19,13 +19,13 @@ export const Register = () => {
             </div>
 
             <div>
-                <label htmlFor="email">E-mail: </label>
-                <input type="text" name='E-mail' placeholder="email" />
+                <label htmlFor="E-mail">E-mail: </label>
+                <input type="text" name='E-mail' placeholder="E-mail" />
             </div>
 
             <div>
                 <label htmlFor="password">Password: </label>
-                <input type="text" name='password' placeholder="Passwoord" />
+                <input type="text" name='password' placeholder="Password" />
             </div>
             <input type="submit" value='Register'/>
         </form>
