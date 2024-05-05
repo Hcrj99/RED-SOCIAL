dafa97ed3b3b679f8d943bb736751cb8ccdbf63e
hcrj99 <hcrj99@hotmail.com>
2024-05-05 14:47:37 -0500

    struct + style register

--- Red_Social/src/Components/Layout/Public/PublicLayout.jsx
+++ Red_Social/src/Components/Layout/Public/PublicLayout.jsx
@@ -1,12 +1,13 @@
 import { Outlet } from 'react-router-dom';
 import { HeaderPublic } from './Header/Header';
+import './Public.css';
 
 export const PublicLayout = () => {
     return (
         <>
             <HeaderPublic />
 
-            <section >
+            <section className='Public'>
                 <Outlet />
             </section>
         </>
--- Red_Social/src/Components/Users/Register/Register.jsx
+++ Red_Social/src/Components/Users/Register/Register.jsx
@@ -1,5 +1,33 @@
+import './Register.css';
+
 export const Register = () => {
     return (
-        <div>Register</div>
+        <form className='Form__register'>
+            <h2>Register Now!</h2>
+            <div> <label htmlFor="Name" >Name: </label>
+                <input type="text" name='Name' placeholder="Name" />
+            </div>
+
+            <div>
+                <label htmlFor="Surname">Surname: </label>
+                <input type="text" name='Surname' placeholder="Surname" />
+            </div>
+
+            <div>
+                <label htmlFor="Nickname">Nickname: </label>
+                <input type="text" name='Nickname' placeholder="Nickname" />
+            </div>
+
+            <div>
+                <label htmlFor="email">E-mail: </label>
+                <input type="text" name='E-mail' placeholder="email" />
+            </div>
+
+            <div>
+                <label htmlFor="password">Password: </label>
+                <input type="text" name='password' placeholder="Passwoord" />
+            </div>
+            <input type="submit" value='Register'/>
+        </form>
     )
 }
