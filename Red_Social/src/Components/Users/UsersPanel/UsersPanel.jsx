5d5b7cb9028789aad513744d5c67c25e87d6458d
hcrj99 <hcrj99@hotmail.com>
2024-05-25 15:03:28 -0500

    get users from backend

--- Red_Social/src/Components/Users/UsersPanel/UsersPanel.jsx
+++ Red_Social/src/Components/Users/UsersPanel/UsersPanel.jsx
@@ -1,22 +1,54 @@
 import userEmpty from '../../../img/userempty.jpg';
 import './UsersPanel.css';
+import { Global } from '../../../Helpers/Global';
+import { useEffect, useState } from 'react';
 
 export const UsersPanel = () => {
+
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        getUsers();
+    }, [])
+
+    const getUsers = async () => {
+        //fecth users
+        const request = await fetch(Global.url + 'user/listprofiles', {
+            method: 'GET',
+            headers: {
+                "contentType": "application/json",
+                "Authorization": localStorage.getItem('token')
+            }
+        });
+
+        const data = await request.json();
+        //state list users
+        if (data.users && data.status === 'success') {
+            setUsers(data.users);
+
+            console.log(users)  //paginate
+        }
+    };
+
     return (
         <section className='users__container'>
-            <article className='user__container-view'>
-                <figure>
-                    <img src={ userEmpty }/>
-                </figure>
-                <div className='user__description'>
-                    <div>nick | name | date</div>
-                    <div>Biography</div>
-                </div>
-                <div className='social__methods'>
-                    <button>FOLLOW</button>
-                    <button>UNFOLLOW</button>
-                </div>
-            </article>
+            {users.map(user => {
+                return (
+                    <article key={user._id} className='user__container-view'>
+                        <figure>
+                            {user.image !== 'Default.png' ? <img src={Global.url + 'user/avatar/' + user.image} alt='user image'></img> : <img src={userEmpty} alt='user image'></img>}
+                        </figure>
+                        <div className='user__description'>
+                            <div>@{user.nick} | {user.name} | date</div>
+                            <div>{user.bio}</div>
+                        </div>
+                        <div className='social__methods'>
+                            <button>FOLLOW</button>
+                            <button>UNFOLLOW</button>
+                        </div>
+                    </article>
+                );
+            })}
         </section>
     )
 }
