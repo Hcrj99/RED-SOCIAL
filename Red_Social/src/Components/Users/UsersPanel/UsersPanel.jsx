805b5c6cb488ecba27b8350a3e85ed18916359fa
hcrj99 <hcrj99@hotmail.com>
2024-05-26 17:55:40 -0500

    delete user logod from panel users

--- Red_Social/src/Components/Users/UsersPanel/UsersPanel.jsx
+++ Red_Social/src/Components/Users/UsersPanel/UsersPanel.jsx
@@ -2,9 +2,10 @@ import userEmpty from '../../../img/userempty.jpg';
 import './UsersPanel.css';
 import { Global } from '../../../Helpers/Global';
 import { useEffect, useState } from 'react';
+import useAuth from '../../../Hooks/useAuth';
 
 export const UsersPanel = () => {
-
+    const { auth } = useAuth();
     const [users, setUsers] = useState([]);
     const [page, setpage] = useState(1);
     const [totalPage, setTotalPages] = useState();
@@ -85,24 +86,28 @@ export const UsersPanel = () => {
     return (
         <section className='users__container'>
             {users.map(user => {
-                return (
-                    <article key={user._id} className='user__container-view'>
-                        <figure>
-                            {user.image !== 'Default.png' ? <img src={Global.url + 'user/avatar/' + user.image} alt='user image'></img> : <img src={userEmpty} alt='user image'></img>}
-                        </figure>
-                        <div className='user__description'>
-                            <div className='user__description-ids'>
-                                <h4 className='nick'>@{user.nick}</h4>
-                                <h4 className='name'>{user.name} date</h4>
-                                <h4 className='bio'>{user.bio}</h4>
+                {if(user._id != auth._id){
+                    return (
+                        <article key={user._id} className='user__container-view'>
+                            <figure>
+                                {user.image !== 'Default.png' ? <img src={Global.url + 'user/avatar/' + user.image} alt='user image'></img> : <img src={userEmpty} alt='user image'></img>}
+                            </figure>
+                            <div className='user__description'>
+                                <div className='user__description-ids'>
+                                    <h4 className='nick'>@{user.nick}</h4>
+                                    <h4 className='name'>{user.name} date</h4>
+                                    <h4 className='bio'>{user.bio}</h4>
+                                </div>
                             </div>
-                        </div>
-                        <div className='social__methods'>
-                            {following.includes(user._id) && <button onClick={() => unFollow(user._id)}>UNFOLLOW</button>}
-                            {!following.includes(user._id) && <button onClick={() => follow(user._id)}>FOLLOW</button>}
-                        </div>
-                    </article>
-                );
+                            {(user._id != auth._id) &&
+                                <div className='social__methods'>
+                                    {following.includes(user._id) && <button onClick={() => unFollow(user._id)}>UNFOLLOW</button>}
+                                    {!following.includes(user._id) && <button onClick={() => follow(user._id)}>FOLLOW</button>}
+                                </div>
+                            }
+                        </article>
+                    );
+                }}
             })}
             <div className='move__paginate-users'>
                 {page > 1 ? <button onClick={prevPage}>Prev</button> : ''}
